3607d54d9d149e49d87b6e59aea67c3f1e7842f2
sinoding <dingje.gm@gmail.com>
2011-04-29 11:12:59 +0800

    this is the commit message

--- test.cc
+++ test.cc
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char **argv) {
   cout << "it is a test of cpp!" << endl;
   cout << "the patch test of git" << endl;
-  
+
+  std::cout << "test of commit --amend" << std::endl;
   return 0;
 }
